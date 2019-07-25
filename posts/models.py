9bac2e60ed7962fb60977e37b346eb2dbb5e612e
Lunc312 <Lunc312@mail.ru>
2019-07-25 14:26:06 +0500

    add carousel

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     title = models.CharField(u'Заголовок', max_length=100)
     content = models.TextField('Текст поста', max_length=300)
